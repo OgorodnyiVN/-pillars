95ffc04da9f53ede637d3d952ba637ada0bb86a2
OgorodnyiVN <100136255+OgorodnyiVN@users.noreply.github.com>
2022-12-12 20:42:32 +0300

    Delete Столбы directory

--- Столбы/js/menu.js
+++
@@ -1,9 +0,0 @@
-(function($){
-    $('.menu-bar').on('click', function() {
-        $('.mobile-menu').toggleClass('triggered');
-    });
-    $('.has-children > a').after('<span></span>'); 
-    $('.has-children span').on('click', function() {
-        $(this).toggleClass('subtriggered');
-    });
-})(jQuery);
